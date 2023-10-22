0fed29f1dd3ef8e18be108414b219ffcb8192e0b
jmacaraeg <jmacaraeg@academic.rrc.ca>
2023-10-22 12:59:08 -0500

    seeded to populate the products table with 676 products with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+676.times do
+  Product.create(
+    title:          Faker::Commerce.product_name,
+    description:    Faker::Lorem.paragraph,
+    price:          Faker::Commerce.price(range: 0..1000.0, as_string: false),
+    stock_quantity: Faker::Number.between(from: 1, to: 100)
+  )
+end
